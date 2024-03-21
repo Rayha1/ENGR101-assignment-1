584a5aa3a20ca3cb8082d80b59dc1abcc3349696
Rayha1 <103225542+Rayha1@users.noreply.github.com>
2024-03-22 12:25:35 +1300

    woowowowowowo

--- third.cpp
+++ third.cpp
@@ -14,12 +14,13 @@ int main(){
                         // - how many samples are there altogether? What type should this variable be? 
    int* waveform = new int[n_samples]; // creates the array 
    int i_sample = 0;
-   int volume = 20000; //max vol value used for equation
-   int frequency;
+   int volume = 0; //max vol value used for equation
+   int frequency = 0;
    int n = 0; //number of notes of same freq added in one go
    int NOTE = 2000; //note duration
-   int REL = 665; //max no# of flat note
+   int REL = 569; //max no# of flat note
    int release = REL; //release note
+   bool decay = false;
    string line;
    ifstream myfile;
    myfile.open("odetojoy.txt");
@@ -38,17 +39,22 @@ int main(){
    for ( int i_sample = 0; i_sample < n_samples ; i_sample++){  
        frequency = waveform[i_sample];
        //xylophone noise
-       if (volume < 19985 && volume > 0){ 
-			   volume = volume - 15; //slow decay
-			   } else {
-				   if (release >= 0) {
-					   volume = 20000; //attack
-					   release--;
+       if (volume < 20000 && decay == false) {
+		   volume = volume + 200;
+		   } else if (volume == 20000 && release > 0){
+			   decay = true;
+			   volume = 20000; //sustain
+			   release--;
+			   }else {
+				   if (volume > 0 && decay == true) {
+					   volume = volume - 20;
 					   } else {
+						   decay = false;
 						   release = REL;
-						   volume = 19984;
-						   } 
-				   } 
+								   }
+						   }
+						  
+				   
 				   
        waveform[i_sample] = volume*sin(dt * i_sample * frequency * 2 * M_PI);// 
        
@@ -56,7 +62,7 @@ int main(){
    // generates sound file from waveform array, writes n_samples numbers  
    // into sound wav file 
    // should know sample_rate for sound timing 
-   MakeWavFromInt("tone1.wav",sample_rate, waveform, n_samples); //file name can be changed but keep extension .wav 
+   MakeWavFromInt("tone3.wav",sample_rate, waveform, n_samples); //file name can be changed but keep extension .wav 
   
    delete[] (waveform); //if using array  
    return 0; 
